bc86c5ea91c394da2fb7db957b355dc55ba523d5
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-15 14:55:35 -0500

    added the model - migration and seeder for the artworks

---
+++ app/Models/Artwork.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artwork extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'image_path',
+        'category_id',
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orders(){
+        return $this->belongsToMany(Order::class,'order_items');
+    }
+
+    public function promotions(){
+        return $this->belongsToMany(Promotion::class, 'artwork_promotions');
+    }
+}
---
+++ database/migrations/2025_01_15_194917_create_artworks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price',10,2)->nullable();
+            $table->string('image_path')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artworks');
+    }
+};
