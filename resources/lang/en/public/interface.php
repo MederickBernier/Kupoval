4dd68d6337b64109556140d88c8cc9ee467ca1a0
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-16 12:18:07 -0500

    added base translations

---
+++ resources/lang/en/public/interface.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'gallery' => 'Gallery',
+    'events' => 'Events',
+    'bio' => 'Bio',
+    'shop' => 'Shop',
+    'about' => 'About',
+    'contact' => 'Contact',
+    'register' => 'Register',
+    'login' => 'Login',
+    'footer_copyright' => 'Copyright &copy; 2025 Kupoval. All Rights Reserved.',
+    'social_linkedin' => 'LinkedIn',
+    'social_facebook' => 'Facebook',
+    'social_instagram' => 'Instagram',
+];
