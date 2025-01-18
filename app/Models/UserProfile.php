9e6d4dad1b5d0ab28df47e64bdc12eb9b0c85f13
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-01-18 09:22:35 -0500

    made some changes, i turned away from seeders and factories

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\User;
 
 // Ensure the User model exists
@@ -13,7 +14,7 @@ if (!class_exists(User::class)) {
 
 class UserProfile extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
