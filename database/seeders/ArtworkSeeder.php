df7c179d30f4bdcbf533f73477ce26b7c38606ea
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-02-26 15:37:28 -0500

    made the carousel better and a 10 artworks random seeder

--- database/seeders/ArtworkSeeder.php
+++ database/seeders/ArtworkSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Artwork;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class ArtworkSeeder extends Seeder
 {
@@ -14,61 +15,42 @@ class ArtworkSeeder extends Seeder
      */
     public function run(): void
     {
-        Artwork::create([
-            'artist_id' => 1,
-            'name' => 'Test 1',
-            'slug' => Str::slug('Test 1'), // Generate the slug
-            'description' => 'Description 1',
-            'height' => 50.5,
-            'width' => 40.2,
-            'initial_price' => 150.00,
-            'image' => 'artworks/test1.jpg',
-            'is_on_sale' => true,
-            'is_featured' => false,
-            'is_for_event' => false,
-            'event_id' => null,
-        ]);
-        Artwork::create([
-            'artist_id' => 1,
-            'name' => 'Test 2',
-            'slug' => Str::slug('Test 2'), // Generate the slug
-            'description' => 'Description 2',
-            'height' => 50.5,
-            'width' => 40.2,
-            'initial_price' => 150.00,
-            'image' => 'artworks/test2.jpg',
-            'is_on_sale' => true,
-            'is_featured' => false,
-            'is_for_event' => false,
-            'event_id' => null,
-        ]);
-        Artwork::create([
-            'artist_id' => 1,
-            'name' => 'Test 3',
-            'slug' => Str::slug('Test 3'), // Generate the slug
-            'description' => 'Description 3',
-            'height' => 50.5,
-            'width' => 40.2,
-            'initial_price' => 150.00,
-            'image' => 'artworks/test3.jpg',
-            'is_on_sale' => true,
-            'is_featured' => false,
-            'is_for_event' => false,
-            'event_id' => null,
-        ]);
-        Artwork::create([
-            'artist_id' => 1,
-            'name' => 'Test 4',
-            'slug' => Str::slug('Test 4'), // Generate the slug
-            'description' => 'Description 4',
-            'height' => 50.5,
-            'width' => 40.2,
-            'initial_price' => 150.00,
-            'image' => 'artworks/test4.jpg',
-            'is_on_sale' => true,
-            'is_featured' => false,
-            'is_for_event' => false,
-            'event_id' => null,
-        ]);
+        $faker = Faker::create();
+
+        // Artwork names that will be used
+        $artworkNames = [
+            'Sunset Dreams',
+            'Urban Symphony',
+            'Whispers of Nature',
+            'Celestial Dance',
+            'Abstract Emotions',
+            'Ocean Memories',
+            'Vibrant Reflections',
+            'Forest Silence',
+            'Midnight Glow',
+            'Mountain Echo'
+        ];
+
+        // Generate 10 randomized artworks
+        for ($i = 1; $i <= 10; $i++) {
+            // Get the corresponding name from the array
+            $name = $artworkNames[$i - 1];
+
+            // Create randomized artwork
+            Artwork::create([
+                'artist_id' => 1, // Stable as requested
+                'name' => $name,
+                'slug' => Str::slug($name),
+                'description' => $faker->paragraph(rand(2, 5)),
+                'height' => $faker->randomFloat(1, 20.0, 120.0),
+                'width' => $faker->randomFloat(1, 20.0, 100.0),
+                'initial_price' => $faker->randomFloat(2, 50.00, 1500.00),
+                'image' => 'artworks/test' . $i . '.jpg', // Following the pattern test1.jpg through test10.jpg
+                'is_on_sale' => $faker->boolean(70), // 70% chance of being on sale
+                'is_featured' => $faker->boolean(30), // 30% chance of being featured
+                'is_for_event' => $isForEvent = $faker->boolean(20), // 20% chance of being for an event
+                'event_id' => $isForEvent ? rand(1, 5) : null,
+            ]);
+        }
     }
 }
--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,14 +1,25 @@
-<div class="glide my-8">
+<div class="glide my-8 relative">
     <!-- Track -->
     <div class="glide__track" data-glide-el="track">
         <ul class="glide__slides">
             @foreach ($carouselItems as $item)
                 <li class="glide__slide">
                     <div class="relative">
-                        <img src="{{ asset('storage/' . $item['image']) }}" alt="{{ $item['title'] }}" class="w-full h-128 object-cover rounded-lg">
+                        <img src="{{ asset('storage/' . $item['image']) }}" alt="{{ $item['name'] }}" class="w-full h-128 object-cover rounded-lg">
                         <div class="absolute bottom-0 left-0 w-full bg-white bg-opacity-80 p-4">
-                            <h3 class="text-xl font-title text-heading">{{ $item['title'] }}</h3>
-                            <p class="text-sm text-body">{!! $item['description'] !!}</p>
+                            <h3 class="text-xl font-title text-heading">{{ $item['name'] }}</h3>
+                            <p class="text-sm text-body mb-6">
+                                {!! Str::limit($item['description'], 120) !!}
+                            </p>
+
+                            <!-- Pagination - centered within the text overlay -->
+                            <div class="flex justify-center pb-2">
+                                <div class="glide__bullets flex space-x-2" data-glide-el="controls[nav]">
+                                    @foreach ($carouselItems as $index => $item)
+                                        <button class="glide__bullet w-3 h-3 rounded-full bg-gray-400 hover:bg-gray-600 focus:bg-gray-600" data-glide-dir="={{ $index }}"></button>
+                                    @endforeach
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </li>
@@ -20,12 +31,5 @@
     <div class="glide__arrows" data-glide-el="controls">
         <button class="glide__arrow glide__arrow--left" data-glide-dir="<">←</button>
         <button class="glide__arrow glide__arrow--right" data-glide-dir=">">→</button>
-    </div>
-
-    <!-- Pagination -->
-    <div class="glide__bullets" data-glide-el="controls[nav]">
-        @foreach ($carouselItems as $index => $item)
-            <button class="glide__bullet" data-glide-dir="={{ $index }}"></button>
-        @endforeach
     </div>
 </div>
